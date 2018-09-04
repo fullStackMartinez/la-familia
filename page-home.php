424dd2f76b9fea6b570c373b142e1300b2b59368
Esteban Martinez <fullstackmartinez@gmail.com>
2018-09-04 13:46:17 -0600

    created page-home.php that will be used for homepage template

---
+++ page-home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Home
+ */?>
+
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+
+
+
+
+
+
+
+	</main>
+</div>
+
+<?php get_footer(); ?>
